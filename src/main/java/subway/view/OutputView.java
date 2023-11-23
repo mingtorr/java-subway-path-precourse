e09b93cd07826dc96f80add73c5bea675e68e75d
mingtorr <mingtorr@gmail.com>
2023-11-23 12:28:07 +0900

    feat: 경로 기준 목록을 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,17 +1,20 @@
 package subway.view;
 
 import subway.domain.MainMenu;
+import subway.domain.PathCriteria;
 
 public class OutputView {
 
     private static final String MSG_MENU_SELECTION = "## 원하는 기능을 선택하세요.";
+    private static final String MSG_PRINT_MAIN_MENU = "## 메인 화면";
+    private static final String MSG_PRINT_PATH_CRITERIA = "## 경로 기준";
 
     public static void printErrorMessage(String message) {
         System.out.println(message);
     }
 
     public static void printMainMenuList() {
-        System.out.println("## 메인 화면");
+        System.out.println(MSG_PRINT_MAIN_MENU);
         MainMenu[] values = MainMenu.values();
 
         for (MainMenu menu : values) {
@@ -21,6 +24,20 @@ public class OutputView {
     }
 
     public static void printMenuSelection() {
+        System.out.println(MSG_MENU_SELECTION);
+    }
+
+    public static void printPathCriteria() {
+        System.out.println(MSG_PRINT_PATH_CRITERIA);
+        PathCriteria[] values = PathCriteria.values();
+
+        for(PathCriteria criteria : values ) {
+            System.out.println(criteria.toString());
+        }
+        System.out.println("\n");
+    }
+
+    public static void printPathCriteriaSelection() {
         System.out.println(MSG_MENU_SELECTION);
     }
 }
